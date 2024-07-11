af11c9ba210404765147043f98deea21cebc8c20
Suffiyan Khan <suffiyanahmed804092@gmail.com>
2024-07-11 17:40:53 +0500

    working

--- src/routes/user.rotes.js
+++ src/routes/user.rotes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { login, logout, updateemail, sendOtpByEmail, updatepassword, saveCertificate, verifyOtp, addPassword } from '../controllers/user.controller.js';
-import { certificategenerate,getAllcertificategenerate } from '../controllers/certificate.controller.js';
+import { certificategenerate, getAllcertificategenerate } from '../controllers/certificate.controller.js';
 import { AllCourse, getAllCours, deletecourse } from '../controllers/courses.controllers.js';
-import { addStudentsData,getAllStudentsData,dummyaddStudentsData } from '../controllers/addStudents.controllers.js';
+import { addStudentsData, getAllStudentsData, dummyaddStudentsData } from '../controllers/addStudents.controllers.js';
 import { getCertificates, searchIussedCertificatyes, sendEmail } from '../controllers/sendEmail.controllers.js';
 const route = express.Router();
-
+route.get('/api', (req, res) => {
+    return res.status(200).json({ status: 200, message: "success" })
+})
 route.post('/saveCertificate', saveCertificate)
 route.post('/adminpassword', addPassword)
 route.post('/login', login)
@@ -21,9 +23,9 @@ route.post('/dummyaddStudents', dummyaddStudentsData)
 route.get('/getAllStudents', getAllStudentsData)
 route.post('/generate', certificategenerate)
 route.get('/getAllCertificates', getAllcertificategenerate)
-route.post('/send-Email',sendEmail)
-route.get("/getallissuedcertificate",getCertificates)
-route.get("/serachissuedcertificate",searchIussedCertificatyes)
+route.post('/send-Email', sendEmail)
+route.get("/getallissuedcertificate", getCertificates)
+route.get("/serachissuedcertificate", searchIussedCertificatyes)
 
 export default route
 
