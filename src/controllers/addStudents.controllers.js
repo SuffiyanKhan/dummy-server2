23e028eba049bc10d4260a54677099d20cbd2874
Suffiyan Khan <suffiyanahmed804092@gmail.com>
2024-07-11 19:34:40 +0500

    working

--- src/controllers/addStudents.controllers.js
+++ src/controllers/addStudents.controllers.js
@@ -36,6 +36,7 @@ const dummyaddStudentsData = async (req, res) => {
 
 const getAllStudentsData = async (req, res) => {
     try {
+        
         // Assuming fetchAllStudentsData() is an async function that fetches data
         const response = await fetchAllStudentsData();
 
